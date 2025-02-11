9960ca479dcbbeed2349437734b9fc6e240f95b0
dev016binarybrix <usamajatt16@gmail.com>
2025-02-11 08:07:26 +0500

    update login and topnav button with dropdown

--- src/app/(admin)/(layout)/slots/Topbar.tsx
+++ src/app/(admin)/(layout)/slots/Topbar.tsx
@@ -12,20 +12,21 @@ import { useRouter } from "next/navigation";
 import { Icon } from "@/components/Icon";
 import { ThemeToggleButton } from "@/components/ThemeToggleButton";
 import {
-    Avatar,
-    Button,
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
-    Navbar,
-    NavbarCenter,
-    NavbarEnd,
-    NavbarStart,
-    maskClassesFn,
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Navbar,
+  NavbarCenter,
+  NavbarEnd,
+  NavbarStart,
+  maskClassesFn,
 } from "@/components/daisyui";
 import { useLayoutContext } from "@/contexts/layout";
 import { routes } from "@/lib/routes";
+
 
 import { NotificationButton } from "../components/NotificationButton";
 import { SearchButton } from "../components/SearchButton";
@@ -34,87 +35,136 @@ import { useDispatch } from "react-redux";
 import { useToast } from "@/hooks/use-toast";
 import { updateAuthCookie } from "@/lib/cookie/auth";
 import { userLogout } from "@/redux/authSlice";
+import { useState } from "react";
 
 const Topbar = () => {
-    const { hideLeftbar, state } = useLayoutContext();
-    const [ logout ] = useLogoutMutation();
-    const navigate = useRouter();
-    const dispatch = useDispatch();
-    const toaster = useToast();
+  const { hideLeftbar, state } = useLayoutContext();
+  const [logout] = useLogoutMutation();
+  const navigate = useRouter();
+  const dispatch = useDispatch();
+  const toaster = useToast();
+  const [open, setOpen] = useState(true);
 
-    const onLogout = async () => {
-        logout({})
-            .unwrap()
-            .then(async(payload) => {
-                toaster.success(payload.message);
-                await Promise.allSettled([
-                    updateAuthCookie({ user: undefined }),
-                    dispatch(userLogout()),
-                ]);
-                navigate.push(routes.auth.login)
-            })
-            .catch(async(error) => {
-                toaster.success("Logout successfully...");
-                await updateAuthCookie({ user: undefined });
-                dispatch(userLogout());
-                navigate.push(routes.auth.login)
-            });
-    };
+  const onLogout = async () => {
+    logout({})
+      .unwrap()
+      .then(async (payload) => {
+        toaster.success(payload.message);
+        await Promise.allSettled([
+          updateAuthCookie({ user: undefined }),
+          dispatch(userLogout()),
+        ]);
+        navigate.push(routes.auth.login);
+      })
+      .catch(async (error) => {
+        toaster.success("Logout successfully...");
+        await updateAuthCookie({ user: undefined });
+        dispatch(userLogout());
+        navigate.push(routes.auth.login);
+      });
+  };
 
-    return (
-        <Navbar className="topbar-wrapper z-10 border-b border-base-200 px-3">
-            <NavbarStart className="gap-3">
-                <Button
-                    shape="square"
-                    color="ghost"
-                    size="sm"
-                    aria-label="Leftmenu toggle"
-                    onClick={() => hideLeftbar(!state.leftbar.hide)}>
-                    <Icon icon={menuIcon} className="inline-block" fontSize={20} />
-                </Button>
-                <SearchButton />
-            </NavbarStart>
-            <NavbarCenter></NavbarCenter>
-            <NavbarEnd className="gap-1.5">
-                {/* <ThemeToggleButton shape="circle" color="ghost" size="sm" /> */}
+  return (
+    <Navbar className="topbar-wrapper z-10 border-b border-base-200 px-3">
+      <NavbarStart className="gap-3">
+        <Button
+          shape="square"
+          color="ghost"
+          size="sm"
+          aria-label="Leftmenu toggle"
+          onClick={() => hideLeftbar(!state.leftbar.hide)}
+        >
+          <Icon icon={menuIcon} className="inline-block" fontSize={20} />
+        </Button>
+        <SearchButton />
+      </NavbarStart>
+      <NavbarCenter></NavbarCenter>
+      <NavbarEnd className="gap-1.5">
+        {/* <ThemeToggleButton shape="circle" color="ghost" size="sm" /> */}
 
-                {/* <NotificationButton /> */}
-                <Dropdown vertical="bottom" end>
-                    <DropdownToggle
-                        className="btn btn-ghost rounded-btn px-1.5 hover:bg-base-content/20"
-                        button={false}>
-                        <div className="flex items-center gap-2">
-                            <Avatar
-                                src={avatar1.src}
-                                size={30}
-                                alt="Avatar"
-                                innerClassName={maskClassesFn({ variant: "squircle" })}></Avatar>
+        <div className="w-84">
+      <div className="relative inline-block">
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="flex items-center gap-2 px-4 py-2 border border-[#3E5EFF] text-[#3E5EFF] rounded-md bg-blue-50 hover:bg-blue-100"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 h-5 text-[#3E5EFF]"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M23 4v6h-6"></path>
+            <path d="M1 20v-6h6"></path>
+            <path d="M3.51 9a9 9 0 0 1 14.85-3L23 10"></path>
+            <path d="M20.49 15a9 9 0 0 1-14.85 3L1 14"></path>
+          </svg>
+          <span className="font-bold text-md">PKR 2,333,854.51</span>
+        </button>
 
-                        </div>
-                    </DropdownToggle>
-                    <DropdownMenu className="mt-4 w-52">
-                        <DropdownItem anchor={false}>
-                            <div>
-                                <Icon icon={userIcon} fontSize={16} /> My Profile
-                            </div>
-                        </DropdownItem>
-                        <DropdownItem anchor={false}>
-                            <div>
-                                <Icon icon={bellIcon} fontSize={16} /> Notification
-                            </div>
-                        </DropdownItem>
-                        <hr className="-mx-2 my-1 border-base-content/10" />
-                        <DropdownItem anchor={false}>
-                            <div className="text-error" onClick={onLogout}>
-                                <Icon icon={logoutIcon} fontSize={16} />
-                                Logout
-                            </div>
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </NavbarEnd>
-        </Navbar>
-    );
+        <div
+          className={`absolute left-0 mt-2 w-48 bg-white border border-gray-200 shadow-lg rounded-lg transition-all duration-300 ease-in-out ${
+            open ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
+          } overflow-hidden`}
+        >
+          <ul className="py-2">
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              Option 1
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              Option 2
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              Option 3
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+       
+
+        {/* <NotificationButton /> */}
+        <Dropdown vertical="bottom" end>
+          <DropdownToggle
+            className="btn btn-ghost rounded-btn px-1.5 hover:bg-base-content/20"
+            button={false}
+          >
+            <div className="flex items-center gap-2">
+              <Avatar
+                src={avatar1.src}
+                size={30}
+                alt="Avatar"
+                innerClassName={maskClassesFn({ variant: "squircle" })}
+              ></Avatar>
+            </div>
+          </DropdownToggle>
+          <DropdownMenu className="mt-4 w-52">
+            <DropdownItem anchor={false}>
+              <div>
+                <Icon icon={userIcon} fontSize={16} /> My Profile
+              </div>
+            </DropdownItem>
+            <DropdownItem anchor={false}>
+              <div>
+                <Icon icon={bellIcon} fontSize={16} /> Notification
+              </div>
+            </DropdownItem>
+            <hr className="-mx-2 my-1 border-base-content/10" />
+            <DropdownItem anchor={false}>
+              <div className="text-error" onClick={onLogout}>
+                <Icon icon={logoutIcon} fontSize={16} />
+                Logout
+              </div>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarEnd>
+    </Navbar>
+  );
 };
 
 export { Topbar };
--- src/app/LoginAuth.tsx
+++ src/app/LoginAuth.tsx
@@ -23,128 +23,154 @@ import { useDispatch } from "react-redux";
 import { updateAuthCookie } from "@/lib/cookie/auth";
 import { Logo } from "@/components/Logo";
 import { off } from "process";
+import authbg from "@/assets/images/auth/auth-bg.png";
 
 const LoginAuth = ({ redirectTo }: { redirectTo?: string }) => {
+  const router = useRouter();
+  const toaster = useToast();
+  const dispatch = useDispatch();
+  const [isdisable, setIsDisable] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const router = useRouter();
-    const toaster = useToast();
-    const dispatch = useDispatch();
-    const [isdisable, setIsDisable] = useState<boolean>(false);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+  const loginSchema = z.object({
+    email: z.string().email("Please enter a valid email address"),
+    password: z
+      .string()
+      .min(5, "Password must be at least 5 characters")
+      .max(15, "Password must not exceed 8 characters"),
+  });
 
-    const loginSchema = z.object({
-        email: z.string().email("Please enter a valid email address"),
-        password: z.string()
-            .min(5, "Password must be at least 5 characters").max(15, "Password must not exceed 8 characters"),
-    });
+  type LoginSchemaType = z.infer<typeof loginSchema>;
 
-    type LoginSchemaType = z.infer<typeof loginSchema>;
+  const { control, handleSubmit, setError } = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "headoffice@gmail.com",
+      password: "admin@test.com",
+    },
+  });
 
-    const { control, handleSubmit, setError } = useForm<LoginSchemaType>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            email: 'headoffice@gmail.com',
-            password: 'admin@test.com',
-        },
-    });
-
-    const setErrors = (errors: Record<string, any>) => {
-        Object.entries(errors).forEach(([key, value]: any[]) => setError(key, { message: value }));
-    };
-
-    const [loginUser] = useLoginMutation();
-
-    const onSubmit = handleSubmit(async (data) => {
-        setIsDisable(true);
-        setIsLoading(true);
-        await loginUser(data).then(async (response:any) => {
-            console.log(response)
-            if("error" in response){
-                setErrors(response.error.data.errors);
-                setIsDisable(false);
-                setIsLoading(false);
-                return;
-            }
-
-            const userDetail = response.data.data;
-            if (response.data.code == 200) {    
-                await Promise.allSettled([
-                    dispatch(loggedIn({ userDetail })),
-                    updateAuthCookie({ user: userDetail?.user })
-                ]).then(()=>{
-                    setIsDisable(false);
-                    setIsLoading(false);
-                    toaster.success("Login successfully...");
-                    router.push(redirectTo ?? routes.home);
-                });
-            }
-        });
-    });
-    return (
-        <>
-
-
-
-            <div className="flex items-center justify-center min-h-screen bg-gray-50">
-                <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
-                    <div className="flex items-center justify-between ml-[120px]">
-                        <Logo />
-                    </div>
-                    <h3 className="text-center text-xl font-semibold md:mt-6 lg:mt-5 mb-3">Wellcome to FlyGlobe</h3>
-                    <h3 className="text-center text-md text-base-content/90 mb-5">
-                        Sign in to your account to continue selling...
-                    </h3>
-                    <div>
-                        <div className="form-control">
-                            <FormLabel title={"Email Address"} htmlFor="email" />
-                            <FormInput
-                                size="md"
-                                startIcon={<Icon icon={mailIcon} className="text-base-content/80" fontSize={18} />}
-                                control={control}
-                                name={"email"}
-                                id="email"
-                                placeholder="Email Address"
-                                className="w-full focus:border-transparent focus:outline-0"
-                                bordered={false}
-                                borderOffset={false}></FormInput>
-                        </div>
-                        <div className="form-control">
-                            <FormLabel title={"Password"} htmlFor="password" />
-                            <FormInputPassword
-                                size="md"
-                                startIcon={<Icon icon={keyRoundIcon} className="text-base-content/80" fontSize={18} />}
-                                control={control}
-                                name={"password"}
-                                id="password"
-                                placeholder="Password"
-                                className="w-full focus:border-transparent focus:outline-0"
-                                bordered={false}
-                                borderOffset={false}></FormInputPassword>
-
-                            <label className="label">
-                                <span className="label-text"></span>
-                                <Link className="label-text text-xs text-base-content/80" href={routes.auth.forgotPassword}>
-                                    Forgot Password?
-                                </Link>
-                            </label>
-                        </div>
-                    </div>
-                    <div className="mt-4 md:mt-4">
-                        <Button
-                            color="primary"
-                            loading={isLoading}
-                            disabled={isdisable}
-                            onClick={onSubmit}
-                            className="gap-3 text-base"
-                            fullWidth
-                            startIcon={<Icon icon={logInIcon} fontSize={16}/>}>
-                            Login
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </>
+  const setErrors = (errors: Record<string, any>) => {
+    Object.entries(errors).forEach(([key, value]: any[]) =>
+      setError(key, { message: value })
     );
+  };
+
+  const [loginUser] = useLoginMutation();
+
+  const onSubmit = handleSubmit(async (data) => {
+    setIsDisable(true);
+    setIsLoading(true);
+    await loginUser(data).then(async (response: any) => {
+      console.log(response);
+      if ("error" in response) {
+        setErrors(response.error.data.errors);
+        setIsDisable(false);
+        setIsLoading(false);
+        return;
+      }
+
+      const userDetail = response.data.data;
+      if (response.data.code == 200) {
+        await Promise.allSettled([
+          dispatch(loggedIn({ userDetail })),
+          updateAuthCookie({ user: userDetail?.user }),
+        ]).then(() => {
+          setIsDisable(false);
+          setIsLoading(false);
+          toaster.success("Login successfully...");
+          router.push(redirectTo ?? routes.home);
+        });
+      }
+    });
+  });
+  return (
+    <>
+      {/* <div className="flex items-center justify-center min-h-screen bg-gray-50"> */}
+    
+      <div
+        className="flex items-center justify-center min-h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(/media/images/auth-bg.png)` }}
+      >
+        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
+          <div className="flex items-center justify-between ml-[120px]">
+            <Logo />
+          </div>
+          <h3 className="text-center text-xl font-semibold md:mt-6 lg:mt-5 mb-3">
+            Wellcome to FlyGlobe
+          </h3>
+          <h3 className="text-center text-md text-base-content/90 mb-5">
+            Sign in to your account to continue selling...
+          </h3>
+          <div>
+            <div className="form-control">
+              <FormLabel title={"Email Address"} htmlFor="email" />
+              <FormInput
+                size="md"
+                startIcon={
+                  <Icon
+                    icon={mailIcon}
+                    className="text-base-content/80"
+                    fontSize={18}
+                  />
+                }
+                control={control}
+                name={"email"}
+                id="email"
+                placeholder="Email Address"
+                className="w-full focus:border-transparent focus:outline-0"
+                bordered={false}
+                borderOffset={false}
+              ></FormInput>
+            </div>
+            <div className="form-control">
+              <FormLabel title={"Password"} htmlFor="password" />
+              <FormInputPassword
+                size="md"
+                startIcon={
+                  <Icon
+                    icon={keyRoundIcon}
+                    className="text-base-content/80"
+                    fontSize={18}
+                  />
+                }
+                control={control}
+                name={"password"}
+                id="password"
+                placeholder="Password"
+                className="w-full focus:border-transparent focus:outline-0"
+                bordered={false}
+                borderOffset={false}
+              ></FormInputPassword>
+
+              <label className="label">
+                <span className="label-text"></span>
+                <Link
+                  className="label-text text-xs text-base-content/80"
+                  href={routes.auth.forgotPassword}
+                >
+                  Forgot Password?
+                </Link>
+              </label>
+            </div>
+          </div>
+          <div className="mt-4 md:mt-4">
+            <Button
+              color="primary"
+              loading={isLoading}
+              disabled={isdisable}
+              onClick={onSubmit}
+              className="gap-3 text-base"
+              fullWidth
+              startIcon={<Icon icon={logInIcon} fontSize={16} />}
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export { LoginAuth };
